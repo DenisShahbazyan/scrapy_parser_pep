7eb6fc24552a7c07b651cc5918734bb605ffa9cf
Denis Shahbazyan <den.shahbazyan@yandex.ru>
2022-07-10 00:32:57 +0300

    Спарсил все данные из главной таблицы

--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -1,5 +1,14 @@
 import scrapy
 
+from pep_parse.items import PepParseItem
+# scrapy startproject pep_parse .
+# scrapy genspider pep peps.python.org
+# scrapy crawl pep
+# scrapy shell "http://peps.python.org/"
+
+
+# В первый файл нужно вывести список всех PEP: номер, название и статус.
+
 
 class PepSpider(scrapy.Spider):
     name = 'pep'
@@ -7,4 +16,11 @@ class PepSpider(scrapy.Spider):
     start_urls = ['http://peps.python.org/']
 
     def parse(self, response):
-        pass
+        rows = response.css('section#numerical-index tbody tr')
+        for row in rows:
+            data = {
+                'status': row.css('td::text').get()[1:],
+                'number': row.css('td + td a::text').get(),
+                'name': row.css('td + td + td a::text').get(),
+            }
+            yield PepParseItem(data)
